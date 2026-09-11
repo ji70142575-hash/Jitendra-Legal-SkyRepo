a8392546095f3ab0f50867d04f6b4e888ddf7b6f
ji70142575-hash <ji70142575@gmail.com>
2026-09-11 11:37:56 +0530

    Create plugin.js

---
+++ tvshows/plugin.js
@@ -0,0 +1,15 @@
+async function getHome() {
+  return [];
+}
+
+async function search(query) {
+  return [];
+}
+
+async function load(url) {
+  return null;
+}
+
+async function loadStreams(url) {
+  return [];
+}
